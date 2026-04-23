4c77aaeb689bf0531061044e4bd7c37c11bec4da
Luís Pacheco <30757322+xaviluisxavier@users.noreply.github.com>
2026-04-23 11:25:05 +0000

    Add files via upload

---
+++ cliente/__main__.py
@@ -0,0 +1,9 @@
+from cliente.interface.interface import Interface
+
+def main():
+    print("A iniciar o cliente Flappy Priolo.")
+    interface_cliente = Interface()
+    interface_cliente.execute()
+
+if __name__ == '__main__':
+    main()
